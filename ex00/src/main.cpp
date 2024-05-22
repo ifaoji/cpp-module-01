389769b8cd3bb9de95fcc0cc3c1a8ab9b781ff9e
Simon Dablander <ifaoji@pm.me>
2024-05-22 19:18:49 +0200

    feat(ex00): nicer log messages

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,16 +1,21 @@
+#include <iostream>
+
 #include "Zombie.hpp"
 
 int main() {
+  std::cout << "Main Simple Test" << std::endl;
   {
     Zombie zom("Hubert");
     zom.announce();
   }
 
+  std::cout << std::endl << "newZombie / Heap Test" << std::endl;
   {
     Zombie *zom = newZombie("Bernhart");
     zom->announce();
     delete zom;
   }
 
+  std::cout << std::endl << "randomChump / Stack Test" << std::endl;
   { randomChump("Chunky"); }
 }
